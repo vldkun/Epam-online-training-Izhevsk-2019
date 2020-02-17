568305cd9875bd2f6149f71245fa3153ffacabf1
Влад Кузьмин <vladkuzmin997@gmail.com>
2020-02-17 20:58:31 +0400

    Module13

---
+++ Module13/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Module13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Students test viewer.");
+
+            var filePath = ReadPath();
+
+            
+            var testsTable = ReadTestsData(filePath);
+            IEnumerable<TestResult> query = testsTable.Select(t=>t.Value);
+            Console.WriteLine(
+                "Choose column to sort by.\nPress button 1 to sort by student name, 2 - by test name,\n 3 - by time of test, 4 - by score.");
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine("\nSorting by student name.");
+                    query = query.OrderBy(t => t.StudentName);
+                    break;
+                }
+
+                if (key.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("\nSorting by test name.");
+                    query = query.OrderBy(t => t.TestName);
+                    break;
+                }
+
+                if (key.Key == ConsoleKey.D3)
+                {
+                    Console.WriteLine("\nSorting by time of test.");
+                    query = query.OrderBy(t => t.TestDate);
+                    break;
+                }
+
+                if (key.Key == ConsoleKey.D4)
+                {
+                    Console.WriteLine("\nSorting by by score.");
+                    query = query.OrderBy(t => t.TestScore);
+                    break;
+                }
+
+                Console.WriteLine("Incorrect column, try again.");
+            }
+
+            
+
+            Console.WriteLine(
+                "Choose sorting order.\nPress button 1 to select ascending order, 2 - by descending order.");
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine("\nSorting in ascending order.");
+                    break;
+                }
+
+                if (key.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("\nSorting in descending order.");
+                    query = query.OrderByDescending(t => t);
+                    break;
+                }
+
+                Console.WriteLine("Incorrect button, try again.");
+            }
+
+            Console.WriteLine(
+                "Enter the number of table lines to display.");
+            while (true)
+            {
+                var num = Console.ReadLine();
+                var flag = true;
+                foreach (var ch in num)
+                {
+                    if (!Char.IsDigit(ch))
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag)
+                {
+                    Console.WriteLine("\nSorting in descending order.");
+                    query = query.Take(Convert.ToInt32(num));
+                    break;
+                }
+
+                Console.WriteLine("Incorrect number, try again.");
+            }
+
+            Console.WriteLine("{0,20} | {1,15} | {2,8} | {3,1}", "Student name", "Test name", "Test date",
+                "Test score");
+            foreach (var line in query)
+            {
+                Console.WriteLine("{0,20} | {1,15} | {2,8} | {3,1}", line.StudentName, line.TestName,
+                    line.TestDate.ToString(CultureInfo.CurrentCulture), line.TestScore);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static string ReadPath()
+        {
+            string filePath;
+            Console.WriteLine("\nEnter path of tests data file.");
+            while (true)
+            {
+                filePath = Console.ReadLine();
+                if (File.Exists(filePath))
+                {
+                    break;
+                }
+
+                Console.WriteLine("File not found. Check the file name and try again.");
+            }
+
+            return filePath;
+        }
+
+        private static SortedDictionary<int,TestResult> ReadTestsData(string path)
+        {
+            var testsTable = new SortedDictionary<int, TestResult>();
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(path)))
+            {
+                int id = 0;
+                while (reader.PeekChar() > -1)
+                {
+                    var testResult = new TestResult(reader.ReadString(), reader.ReadString(),
+                        DateTime.Parse(reader.ReadString()), reader.ReadString());
+                    testsTable.Add(id, testResult);
+                    id++;
+                }
+            }
+
+            return testsTable;
+        }
+    }
+}
---
+++ Module13/TestResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Module13
+{
+    public class TestResult
+    {
+        public string StudentName { get; set; }
+        public string TestName { get; set; }
+        public DateTime TestDate { get; set; }
+        public string TestScore { get; set; }
+
+        public TestResult(string student, string test, DateTime date, string score)
+        {
+            StudentName = student;
+            TestName = test;
+            TestDate = date;
+            TestScore = score;
+        }
+    }
+}
